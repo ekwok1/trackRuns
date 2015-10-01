fafc50be92d89edcb1c7929ce522ec4ecc135dae
Eugene Kwok <eugenekwok11@gmail.com>
2015-10-01 14:15:31 -0700

    middleware routeHelper

---
+++ middleware/routeHelper.js
@@ -0,0 +1,23 @@
+var db = require("../models");
+
+var routeHelpers = {
+  ensureLoggedIn: function(req, res, next) {
+    if (req.session.id !== null && req.session.id !== undefined) {
+      return next();
+    }
+    else {
+     res.redirect('/login');
+    }
+  },
+
+  preventLoginSignup: function(req, res, next) {
+    if (req.session.id !== null && req.session.id !== undefined) {
+      res.redirect('/');
+    }
+    else {
+     return next();
+    }
+  }
+};
+
+module.exports = routeHelpers;
